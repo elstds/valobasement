309d4218d13df911f6478cb0583b2d15f9dd5f01
excretory <elstxds@proton.me>
2026-04-15 20:28:47 +0500

    Added base character class

---
+++ src/character.cpp
@@ -0,0 +1,36 @@
+#include "character.h"
+#include <SFML/Graphics/Sprite.hpp>
+
+Character::~Character() {}
+
+void Character::takeDamage(float damage) {
+    m_health -= damage;
+}
+
+void Character::setPosition(sf::Vector2f& pos) {
+    m_pos = pos;
+}
+
+void Character::setDirection(Direction direction) {
+    m_direction = direction;
+}
+
+float Character::getHP() const {
+    return m_health;
+}
+
+sf::Vector2f Character::getSize() const {
+    return m_size;
+}
+
+sf::Vector2f Character::getPosition() const {
+    return m_pos;
+}
+
+sf::Sprite Character::getSprite() const {
+    return getSprite();
+}
+
+Direction Character::getDirection() const {
+    return m_direction;
+}
